841d60db26329320962a69e35fa0eeed6c6f591b
Thiago Seus <thiago.seus@yahoo.com.br>
2022-05-22 19:49:47 -0300

    Adicionada primeira versão dos arquivos da pipeline (teste local)

---
+++ pipeline/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./cross/lib/fpc/3.2.2/ppcross386 @pipeline/fpc.cfg -dRELEASE -Twin32 -Mdelphi src/edivox.dpr -Fudvtradutor/src/lianetts -Fudvtradutor/src/tradutor
---
+++ pipeline/download-compiler.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl http://localhost/fpc-cross.tar.xz | tar -Jx --
